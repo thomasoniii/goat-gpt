ef6641b62443c577e1d58ec966b747d50502ec4d
Jim Thomason <jim@japanesewartuba.com>
2024-06-13 15:46:04 -0700

    jimt/goat-gpt - added dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect, useMemo } from "react";
 import "./App.css";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
-import Container from "@mui/material/Container";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 import UserIcon from "@mui/icons-material/Person";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
@@ -11,11 +12,39 @@ import ListItem from "@mui/material/ListItem";
 
 import "@fontsource/roboto/300.css";
 
+const useThemeDetector = () => {
+  const getCurrentTheme = () =>
+    window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const [isDarkTheme, setIsDarkTheme] = useState(getCurrentTheme());
+  const mqListener = (e) => {
+    setIsDarkTheme(e.matches);
+  };
+
+  useEffect(() => {
+    const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
+    darkThemeMq.addListener(mqListener);
+    return () => darkThemeMq.removeListener(mqListener);
+  }, []);
+  return isDarkTheme ? "dark" : "light";
+};
+
 function App() {
   const [msg, setMsg] = useState("");
   const [msgs, addMsg] = useState([]);
   const key = useRef(0);
   const scrollRef = useRef(null);
+
+  const currentTheme = useThemeDetector();
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: currentTheme,
+        },
+      }),
+    [currentTheme]
+  );
 
   useEffect(() => {
     if (scrollRef.current) {
@@ -40,69 +69,73 @@ function App() {
   const GoatIcon = <img src="/goat-gpt/goat.jpg" width="32" height="32" />;
 
   return (
-    <Box>
-      <Typography variant="h2" gutterBottom align="center">
-        Goat GPT
-      </Typography>
-      <List
-        component="section"
-        sx={{
-          p: 2,
-          border: "1px dashed grey",
-          height: "500px",
-          overflow: "scroll",
-          width: "500px",
-          mb: 1,
-        }}
-      >
-        {msgs.map((msg) => (
-          <ListItem
-            key={msg.key}
-            sx={{
-              display: "flex",
-              justifyContent: msg.type === "goat" ? "flex-end" : "flex-start",
-              backgroundColor: msg.type === "goat" ? "#DDFFDD" : "#FFDDFF",
-            }}
-          >
-            {msg.type === "goat" ? "" : <UserIcon />}
-            {msg.msg}
-            {msg.type === "goat" ? GoatIcon : ""}
-          </ListItem>
-        ))}
-        <ListItem ref={scrollRef} />
-      </List>
-      <Box
-        sx={{
-          border: "1px solid black",
-          p: 1,
-          display: "flex",
-          justifyContent: "space-between",
-        }}
-      >
-        <TextField
-          sx={{ m: 1 }}
-          size="small"
-          fullWidth
-          value={msg}
-          onChange={(e) => setMsg(e.target.value)}
-          onKeyDown={(e) => {
-            if (e.key === "Enter") {
-              appendMsg();
-            }
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Box>
+        <Typography variant="h2" gutterBottom align="center">
+          Goat GPT
+        </Typography>
+        <List
+          component="section"
+          sx={{
+            p: 2,
+            border: "1px dashed grey",
+            height: "500px",
+            overflow: "scroll",
+            width: "500px",
+            mb: 1,
           }}
-          autoFocus
-        />
+        >
+          {msgs.map((msg) => (
+            <ListItem
+              key={msg.key}
+              sx={{
+                display: "flex",
+                justifyContent: msg.type === "goat" ? "flex-end" : "flex-start",
+                color: "black",
+                backgroundColor: msg.type === "goat" ? "#DDFFDD" : "#FFDDFF",
+              }}
+            >
+              {msg.type === "goat" ? "" : <UserIcon />}
+              {msg.msg}
+              {msg.type === "goat" ? GoatIcon : ""}
+            </ListItem>
+          ))}
+          <ListItem ref={scrollRef} />
+        </List>
+        <Box
+          sx={{
+            border: "1px solid black",
+            p: 1,
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          <TextField
+            sx={{ m: 1 }}
+            size="small"
+            fullWidth
+            value={msg}
+            onChange={(e) => setMsg(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                appendMsg();
+              }
+            }}
+            autoFocus
+          />
 
-        <Button variant="contained" onClick={appendMsg} sx={{ m: 1 }}>
-          Send
-        </Button>
+          <Button variant="contained" onClick={appendMsg} sx={{ m: 1 }}>
+            Send
+          </Button>
+        </Box>
+        <Box sx={{ textAlign: "right" }}>
+          <a href="https://github.com/thomasoniii/goat-gpt" target="_blank">
+            https://github.com/thomasoniii/goat-gpt
+          </a>
+        </Box>
       </Box>
-      <Box sx={{ textAlign: "right" }}>
-        <a href="https://github.com/thomasoniii/goat-gpt" target="_blank">
-          https://github.com/thomasoniii/goat-gpt
-        </a>
-      </Box>
-    </Box>
+    </ThemeProvider>
   );
 }
 
